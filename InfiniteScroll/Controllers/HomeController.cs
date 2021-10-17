c128f413d9efbb5e098909a3ea5d068402156c03
Sumeet Bhati <bhatisumit10@gmail.com>
2021-10-17 13:59:16 +0530

    Add project files.

---
+++ InfiniteScroll/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using InfiniteScroll.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InfiniteScroll.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Getdata(int pageNumber, int pageSize)
+        {
+            List<Employee> listEmployees = new List<Employee>();
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spGetEmployees", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter()
+                {
+                    ParameterName = "@PageNumber",
+                    Value = pageNumber
+                });
+
+                cmd.Parameters.Add(new SqlParameter()
+                {
+                    ParameterName = "@PageSize",
+                    Value = pageSize
+                });
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.ID = Convert.ToInt32(rdr["Id"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.Gender = rdr["Gender"].ToString();
+                    employee.Salary = Convert.ToInt32(rdr["Salary"]);
+                    listEmployees.Add(employee);
+                }
+            }
+
+            return Json(listEmployees , JsonRequestBehavior.AllowGet);
+        }
+
+   
+    }
+}
